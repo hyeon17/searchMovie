675e0dd2dd4ca52c51de74d2ef8f18d5bb980b9c
hyeon17 <janghyeon8725@naver.com>
2023-05-25 07:02:54 +0900

    conf: ThemeProvider, GlobalStyle 설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from 'styled-components';
+import {theme} from '@/styles/theme.styles.ts';
+import GlobalStyle from '@/styles/Global.styles.ts';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 );
