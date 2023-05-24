41fcfd37f3749b2539023ac60044748a6a1d4a9d
hyeon17 <janghyeon8725@naver.com>
2023-05-25 00:59:40 +0900

    feat: api 추가

---
+++ src/apis/index.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import { AxiosError, AxiosResponse } from 'axios';
+import { ResponseValue, Movie } from '@/types/searchMovie';
+import { ResponseValue as ResponseValueId, Rating } from '@/types/searchMovieId';
+
+export const useSearchMovie = (title: string, year?: number, page?: number) => {
+  const queryKey = ['movies'];
+  const queryFn = async () => {
+    const url = `${import.meta.env.VITE_APP_BASE_URL}&s=${title}&y=${year}&page=${page}`;
+    const response = await axios.get<AxiosResponse<any>>(url);
+    return response.data;
+  };
+
+  return useQuery<AxiosResponse<any>, AxiosError>({
+    queryKey,
+    queryFn,
+  });
+};
+
+export const useSearchMovieId = (id:number) => {
+  const queryKey = ['movieId'];
+  const queryFn = async () => {
+    const url = `${import.meta.env.VITE_APP_BASE_URL}&i=${id}`;
+    const response = await axios.get<AxiosResponse<any>>(url);
+    return response.data;
+  };
+
+  return useQuery<AxiosResponse<any>, AxiosError>({
+    queryKey,
+    queryFn,
+  });
+};
