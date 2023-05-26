7b005d7ad90bc99680169f62cc10fdd45fc329b2
JangHyeonJun <janghyeon8725@naver.com>
2023-05-27 07:56:08 +0900

    fix: Overlay width변경

--- src/styles/Main.styles.ts
+++ src/styles/Main.styles.ts
@@ -3,24 +3,23 @@ import styled from 'styled-components';
 export const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin: 0 auto;
+  margin: 20px auto;
   width: 1200px;
   height: 100vh;
 `;
 
 export const MainWrapper = styled.main`
   background-image: url('../../public/main_back.png');
-  height: 100%;
   width: 100%;
+  height: 100%;
   background-size: cover;
   background-position: center;
 `;
 
 export const Overlay = styled.div`
   position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
+  top: 60px;
+  width: 1200px;
   height: 100vh;
   background-color: rgba(0, 0, 0, 0.5); /* 흐림 효과를 줄 색상과 투명도 설정 */
 `;
