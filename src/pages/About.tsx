478c2f528e7224fecb091fd9936ba8655036d95f
JangHyeonJun <janghyeon8725@naver.com>
2023-05-27 07:53:29 +0900

    feat: card 추가

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
 import * as S from '@/styles/Page.styles';
 import { GithubOutlined } from '@ant-design/icons';
+import Card from '@/components/Card';
+
 function AboutPage() {
   return (
     <S.PageContainer>
-      <S.CardContainer cover={<img src="../../public/profile.png" />}>
-        <S.CardDescription
-          title="Jang Hyeon Jun"
-          description={
-            <a href="https://github.com/hyeon17">
-              <GithubOutlined /> GitHub
-            </a>
-          }
-        />
-      </S.CardContainer>
+      <Card
+        image={<img src="../../public/profile.png" alt='profile' />}
+        title={'Jang Hyeon Jun'}
+        description={
+          <a href="https://github.com/hyeon17">
+            <GithubOutlined /> GitHub
+          </a>
+        }
+      />
     </S.PageContainer>
   );
 }
