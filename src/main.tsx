cb7a7cad26d7d88cc46348190e898b28b49055bd
JangHyeonJun <janghyeon8725@naver.com>
2023-05-27 07:51:52 +0900

    fix: React.StrictMode제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -9,12 +8,10 @@ import GlobalStyle from '@/styles/Global.styles.ts';
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <App />
       </ThemeProvider>
     </QueryClientProvider>
-  </React.StrictMode>,
 );
