a0bb6b6354beebf73ef86ce859eefbe01d022cf3
hyeon17 <janghyeon8725@naver.com>
2023-05-28 01:21:43 +0900

    feat: useInfiniteQuery추가

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useInfiniteQuery } from '@tanstack/react-query';
 import { AxiosError, AxiosResponse } from 'axios';
-import { ResponseValue, Movie } from '@/types/searchMovie';
-import { ResponseValue as ResponseValueId, Rating } from '@/types/searchMovieId';
 
 export const useSearchMovie = (title: string, type?: string, year?: number | string, page?: number | string) => {
   const queryKey = ['movies'];
@@ -31,3 +29,38 @@ export const useSearchMovieId = (id: number) => {
     queryFn,
   });
 };
+
+
+// export const useSearchMovie2 = (title: string, type?: string, year?: number | string) => {
+//   const queryKey = ['movies', title, type, year];
+
+//   const fetchMovies = async ({ pageParam = 1 }) => {
+//     const url = `${import.meta.env.VITE_APP_BASE_URL}&s=${title}&type=${type}&y=${year}&page=${pageParam}`;
+//     const response = await axios.get<AxiosResponse<any>>(url);
+//     return response.data;
+//   };
+
+//   const { data, error, fetchNextPage, hasNextPage, isFetching, isLoading, isError, isSuccess } = useInfiniteQuery<
+//     AxiosResponse<any>,
+//     AxiosError
+//   >({
+//     queryKey,
+//     queryFn: fetchMovies,
+//     getNextPageParam: (lastPage) => {
+//       const { data } = lastPage;
+//       const totalPages = Math.ceil(data?.totalResults / 10);
+//       return currentPage + 1 <= totalPages ? currentPage + 1 : undefined;
+//     },
+//   });
+
+//   return {
+//     data,
+//     error,
+//     fetchNextPage,
+//     hasNextPage,
+//     isFetching,
+//     isLoading,
+//     isError,
+//     isSuccess,
+//   };
+// };
