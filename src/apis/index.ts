4896a492ed562090e291cd5e76cf2adaa46651b9
JangHyeonJun <janghyeon8725@naver.com>
2023-05-29 07:00:07 +0900

    refactor: 반환값 수정, 무한 스크롤 더미 코드 삭제

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useQuery, useInfiniteQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { AxiosError, AxiosResponse } from 'axios';
 
 export const useSearchMovie = (title: string, type?: string, year?: number | string, page?: number | string) => {
@@ -7,12 +7,14 @@ export const useSearchMovie = (title: string, type?: string, year?: number | str
   const queryFn = async () => {
     const url = `${import.meta.env.VITE_APP_BASE_URL}&s=${title}&type=${type}&y=${year}&page=${page}`;
     const response = await axios.get<AxiosResponse<any>>(url);
-    return response.data;
+    return response;
   };
 
   return useQuery<AxiosResponse<any>, AxiosError>({
     queryKey,
     queryFn,
+    keepPreviousData: true,
+    staleTime: 5000,
   });
 };
 
@@ -21,7 +23,7 @@ export const useSearchMovieId = (id: number) => {
   const queryFn = async () => {
     const url = `${import.meta.env.VITE_APP_BASE_URL}&i=${id}`;
     const response = await axios.get<AxiosResponse<any>>(url);
-    return response.data;
+    return response;
   };
 
   return useQuery<AxiosResponse<any>, AxiosError>({
@@ -29,38 +31,3 @@ export const useSearchMovieId = (id: number) => {
     queryFn,
   });
 };
-
-
-// export const useSearchMovie2 = (title: string, type?: string, year?: number | string) => {
-//   const queryKey = ['movies', title, type, year];
-
-//   const fetchMovies = async ({ pageParam = 1 }) => {
-//     const url = `${import.meta.env.VITE_APP_BASE_URL}&s=${title}&type=${type}&y=${year}&page=${pageParam}`;
-//     const response = await axios.get<AxiosResponse<any>>(url);
-//     return response.data;
-//   };
-
-//   const { data, error, fetchNextPage, hasNextPage, isFetching, isLoading, isError, isSuccess } = useInfiniteQuery<
-//     AxiosResponse<any>,
-//     AxiosError
-//   >({
-//     queryKey,
-//     queryFn: fetchMovies,
-//     getNextPageParam: (lastPage) => {
-//       const { data } = lastPage;
-//       const totalPages = Math.ceil(data?.totalResults / 10);
-//       return currentPage + 1 <= totalPages ? currentPage + 1 : undefined;
-//     },
-//   });
-
-//   return {
-//     data,
-//     error,
-//     fetchNextPage,
-//     hasNextPage,
-//     isFetching,
-//     isLoading,
-//     isError,
-//     isSuccess,
-//   };
-// };
