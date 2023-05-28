fbd0f3a9187e1d3f1db5fb8eaf6862808b2f2764
JangHyeonJun <janghyeon8725@naver.com>
2023-05-29 07:01:54 +0900

    feat: 즐겨찾기 추가

--- src/pages/Favorite.tsx
+++ src/pages/Favorite.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Content from '@/components/Content';
+import { useFavoriteStore } from '@/store/favoriteStore';
+import { useSearchMovieId } from '@/apis';
 
 function FavoritePage() {
+  // const { getFid } = useFavoriteStore();
+  // const { data, error, isLoading } = useSearchMovieId(getFid());
+  // todo: 여기서 로컬스토리지 값 가져와서 가공 후 content에 넘김, 또한 조건문을 통해 로컬스토리지에 값이 있다면 true or false 반환해서 item에 넘겨줌
   return (
     <>
-      <div>즐겨찾기</div>
-      <Content content={''} loading={false} />
+      <Content content={''} loading={false} favorite={true} item={false} />
     </>
   );
 }
 
-export default React.memo(FavoritePage);
+export default FavoritePage;
