3d2ce594dc67a2996947163412827b3e8ced7523
JangHyeonJun <janghyeon8725@naver.com>
2023-05-29 06:58:11 +0900

    feat: 즐겨찾기, 스타일 개선, 아이콘 추가, 상세보기 추가

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,9 +1,53 @@
+import React, { useState, useEffect } from 'react';
 import * as S from '@/styles/Card.styles';
+import { HeartOutlined, HeartFilled, ExclamationCircleOutlined } from '@ant-design/icons';
+import { useIdStore } from '@/store/idStore';
+import { useModalStore } from '@/store/modalStore';
+import { useFavoriteStore } from '@/store/favoriteStore';
 
-function Card({ image, title, description, style }: any) {
+function Card({ image, title, description, style, icon, data }: any) {
+  const [heart, setHeart] = useState(false);
+  const { setOpen } = useModalStore();
+  const { setId } = useIdStore();
+  const { setFid } = useFavoriteStore();
+  const [item, setItem] = useState<any[]>([]);
+
+  useEffect(() => {
+    const storedId = localStorage.getItem('fid');
+    if (storedId) {
+      setItem(JSON.parse(storedId));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (item.length > 0) {
+      localStorage.setItem('fid', JSON.stringify(item));
+    }
+  }, [item]);
+
+  const useHandleHeart = (imdbID: number) => {
+    setHeart(!heart);
+    setItem((prev) => [...prev, imdbID]);
+  };
+
+  const handleClickCard = (imdbID: number) => {
+    setId(imdbID);
+    setOpen();
+  };
+
   return (
     <S.CardContainer style={style} cover={image}>
       <S.CardDescription title={title} description={description} />
+      {icon && (
+        <S.CardIcon>
+          <div onClick={() => handleClickCard(data.imdbID)}>
+            <ExclamationCircleOutlined />
+          </div>
+          <S.CardHeart onClick={() => useHandleHeart(data.imdbID)}>
+            {heart ? <HeartFilled /> : <HeartOutlined />}
+          </S.CardHeart>
+        </S.CardIcon>
+      )}
     </S.CardContainer>
   );
 }
