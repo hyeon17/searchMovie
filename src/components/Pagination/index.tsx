f36c6b2d948411880a456ccd67c48460d863036e
hyeon17 <janghyeon8725@naver.com>
2023-05-30 00:05:03 +0900

    feat: 페이지네이션 추가

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,39 +1,25 @@
 import React from 'react';
 import * as S from '@/styles/Content.styles';
-import { useSearchMovie } from '@/apis';
 import { useOptionStore } from '@/store/optionStore';
-import { useResponseStore } from '@/store/responseStore';
 
 function Pagination({ content }: any) {
-  const { getYear, getTitle, getCategory, getCount } = useOptionStore();
-  // const { data, isLoading, isError } = useSearchMovie(getTitle(), getCategory(), getYear(), 1);
-const { setValue, getValue } = useResponseStore();
-  const handlePageChange = (newPage: number) => {
-    // Fetch data for the selected page
-    useSearchMovie(getTitle(), getCategory(), getYear(), newPage);
+  const { setCount } = useOptionStore();
+  const totalResults = content.totalResults || 0;
+  const totalPages = Math.ceil(totalResults / 10); // Assuming 10 items per page
+  const handlePageChange = (page: number) => {
+    setCount(page);
   };
-
-  // if (isLoading) {
-  //   return <div>Loading...</div>;
-  // }
-
-  // if (isError) {
-  //   return <div>Error occurred while fetching data</div>;
-  // }
-
-  const totalResults = content?.totalResults ?? 0;
-  const totalPages = Math.ceil(totalResults / 10);
 
   return (
     <>
       <S.StyledPagination
         defaultCurrent={1}
-        // onChange={handlePageChange}
+        onChange={handlePageChange}
         showTotal={(total: any, range: any) => `${range[0]}-${range[1]} of ${total} items`}
         defaultPageSize={10}
-        showSizeChanger ={false}
+        showSizeChanger={false}
         total={totalResults}
-        hideOnSinglePage={totalPages <= 1} // Hide pagination when there is only one page
+        hideOnSinglePage={totalPages <= 1}
       />
     </>
   );
