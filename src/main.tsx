bd285ce587b11f0253116ad9aa3d292a0bac2e57
hyeon17 <janghyeon8725@naver.com>
2023-05-30 00:04:31 +0900

    conf: react-helmet-async 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,17 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'styled-components';
 import {theme} from '@/styles/theme.styles.ts';
 import GlobalStyle from '@/styles/Global.styles.ts';
+import { HelmetProvider } from 'react-helmet-async';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <HelmetProvider>
         <App />
-      </ThemeProvider>
-    </QueryClientProvider>
+      </HelmetProvider>
+    </ThemeProvider>
+  </QueryClientProvider>,
 );
