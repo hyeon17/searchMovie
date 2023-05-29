f82c9060953503f339eece57cffaeb88016cd503
hyeon17 <janghyeon8725@naver.com>
2023-05-30 00:06:02 +0900

    fix: 타입 수정

--- src/store/optionStore.ts
+++ src/store/optionStore.ts
@@ -3,26 +3,26 @@ import { create } from 'zustand';
 interface optionState {
   year: number | string;
   category: string;
-  Count: number | string;
+  Count: number;
   title: string;
   setYear: (year: number | string) => void;
   setCategory: (category: string) => void;
-  setCount: (Count: number | string) => void;
+  setCount: (Count: number) => void;
   setTitle: (title: string) => void;
   getYear: () => number | string;
   getCategory: () => string;
-  getCount: () => number | string;
+  getCount: () => number;
   getTitle: () => string;
 }
 
 const optionState = create<optionState>((set, get) => ({
   year: '',
   category: '',
-  Count: '',
+  Count: 1,
   title: '',
   setYear: (year: number | string) => set({ year }),
   setCategory: (category: string) => set({ category }),
-  setCount: (Count: number | string) => set({ Count }),
+  setCount: (Count: number) => set({ Count }),
   setTitle: (title: string) => set({ title }),
   getYear: () => get().year,
   getCategory: () => get().category,
