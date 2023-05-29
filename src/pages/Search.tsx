384f658ed5f2e77fc9ad0243ca301ead20a55336
hyeon17 <janghyeon8725@naver.com>
2023-05-30 00:07:25 +0900

    refactor: 코드 리팩토링

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -4,25 +4,45 @@ import Content from '@/components/Content';
 import Search from '@/components/Search';
 import { useSearchMovie } from '@/apis';
 import { useOptionStore } from '@/store/optionStore';
-import { useResponseStore } from '@/store/responseStore';
+import { Helmet } from 'react-helmet-async';
 
 function SearchPage() {
   const { getYear, getTitle, getCategory, getCount } = useOptionStore();
-  const { setValue, getValue } = useResponseStore();
+
+  const {
+    data: res,
+    isLoading,
+    isSuccess,
+    isFetching,
+  } = useSearchMovie(getTitle(), getCategory(), getYear(), getCount());
   const [searchValue, setSearchValue] = useState('');
-  const { data:res, isLoading } = useSearchMovie(getTitle(), getCategory(), getYear(), getCount());
   const response = res?.data;
 
   useEffect(() => {
     if (res && response.Response === 'True') {
-      setValue(response);
       setSearchValue(response);
     }
-  }, [res]);
+  }, [response]);
 
+  // console.log('response', response);
+  // if (response) {
+  //   console.log('res', response.Response);
+  // }
+  // console.log('loading',isLoading);
+  // console.log('success', isSuccess);
+  // console.log('fetching', isFetching);
+
+  // useEffect(() => {
+  // if (isSuccess) {
+  //   setSearchValue(response);
+  // }
+  // }, [response]);
 
   return (
     <>
+      <Helmet>
+        <title>Search | MOVIEFLIX</title>
+      </Helmet>
       <Search />
       <Content content={searchValue} loading={isLoading} favorite={false} />
     </>
