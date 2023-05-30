2ce41894845c7729b77f70287a984a349695ae63
hyeon17 <janghyeon8725@naver.com>
2023-05-30 21:37:15 +0900

    feat: StrictMode 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,23 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'styled-components';
-import {theme} from '@/styles/theme.styles.ts';
+import { theme } from '@/styles/theme.styles.ts';
 import GlobalStyle from '@/styles/Global.styles.ts';
 import { HelmetProvider } from 'react-helmet-async';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <QueryClientProvider client={queryClient}>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </ThemeProvider>
-  </QueryClientProvider>,
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  </React.StrictMode>,
 );
